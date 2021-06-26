7cb5c36a554c1cdd821c529040908133d5998ed8
codeph-0bia <codephobia642@gmail.com>
2021-06-26 12:03:11 +0530

    Added a fake users RESTapi. removed robots.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox.js';
-import { robots } from './robots';
 import './App.css';
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
@@ -20,6 +25,9 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    }
     return (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import 'tachyons';
-import { robots } from './robots.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
