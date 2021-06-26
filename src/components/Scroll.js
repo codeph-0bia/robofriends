f4ffac8af7eaf0cb748e0f7c21c6a29b5aa770df
codeph-0bia <codephobia642@gmail.com>
2021-06-26 12:47:01 +0530

    created organisation

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -5,8 +5,8 @@ const Scroll = (props) => {
     <div
       style={{
         overflowY: 'scroll',
-        overflowX: 'hidden',
         border: '5px solid black',
+        height: '500px',
       }}
     >
       {props.children}
